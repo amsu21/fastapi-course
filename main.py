fef768adbe8c3e998173f9db11730df36fb9b46b
amsu21 <aswb4@hotmail.com>
2023-06-05 17:17:28 -0400

    Refactored 404 code

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import fastapi
-from fastapi import FastAPI, Response, status
+from fastapi import FastAPI, HTTPException, Response, status
 from fastapi.params import Body
 from fastapi.params import Optional
 from pydantic import BaseModel
@@ -45,7 +45,9 @@ def create_posts(new_post: Post):
 def get_post(id: int, response: Response):
     post = find_post(id)
     if not post:
-        response.status_code = status.HTTP_400_BAD_REQUEST
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
+        # response.status_code = status.HTTP_400_BAD_REQUEST
+        # return {'message': f"post with id: {id} was not found"}
     print(post)
     return {"post_detail": post}
 
