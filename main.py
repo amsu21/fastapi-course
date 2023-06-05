b064d11bdfb360728eeaf10d3d79f1e56d3e2924
amsu21 <aswb4@hotmail.com>
2023-06-05 17:38:27 -0400

    Added if statement for when a post does not exist

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from random import randrange
 
 app = FastAPI()
 
-
 # STORES ALL THE POSTS
 my_posts = [{"titile": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "favorite shoes", "content": "New Balances", "id": 2}]
 
@@ -21,7 +20,6 @@ def find_index_post(id):
         if posts['id'] == id:
             return index
         
-
 
 class Post(BaseModel):
     title: str
@@ -66,6 +64,9 @@ def get_post(id: int, response: Response):
 def delete_post(id: int):
     # FIND THE INDEX IN THE ARRAY THAT HAS REQUIRED ID
     index = find_index_post(id)
+    
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
     
     my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
